0a961d57d21811c5c9c73208bfe830ac0e16e3f4
c8656 <chenyufr@gmail.com>
2018-06-17 16:09:57 +0800

    add if and input

---
+++ test-input-if.py
@@ -0,0 +1,9 @@
+num = 10
+print ('what number i think  ?')
+answer = int(input())
+if answer < num:
+    print ("too small")
+if  answer > num:
+    print ("too big")
+if answer == num:
+    print ("OK")
